e5f8090862e7243a30a3ce692b59acbaea2c7403
Woosang <yws1502@naver.com>
2021-08-31 22:08:50 +0900

    update search bar

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Project
+
+from .models import Project, Tag
 from .forms import ProjectForm
+from .utils import search_project
 
 def projects(request):
-    projects = Project.objects.all()
-    context = {'projects' : projects}
+    search_query, projects = search_project(request)
+
+    context = {'projects' : projects, 'search_query':search_query}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
--- users/views.py
+++ users/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 from django.contrib.auth.models import User
+from django.db.models import Q
+
 from .models import Profile, Skill
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .utils import search_profile
 
 def login_user(request):
     page = 'login'
@@ -54,8 +56,9 @@ def register_user(request):
     return render(request, 'users/login_register.html', context)
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles':profiles}
+    profiles, search_query = search_profile(request)
+
+    context = {'profiles':profiles, 'search_query':search_query}
     return render(request, 'users/profiles.html', context)
 
 def user_profile(request, pk):
