a4066a9979e401e4413e4fe1d14722a97d487f3a
Woosang <yws1502@naver.com>
2021-09-03 01:54:39 +0900

    Authentivataion with JWT

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import *
 
 urlpatterns = [
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', get_routes),
     path('projects/', get_projects),
     path('projects/<str:pk>/', get_project),
+
 ]
