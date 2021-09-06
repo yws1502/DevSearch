5136be7395e39affedfefc524665c9fd4d48a05f
Woosang <yws1502@naver.com>
2021-09-06 15:08:05 +0900

    move directory

--- api/serializers.py
+++ api/serializers.py
@@ -22,6 +22,9 @@ class ProjectSerializer(serializers.ModelSerializer):
     owner = ProfileSerializer(many=False)
     tags = TagSerializer(many=True)
     reviews = serializers.SerializerMethodField()
+    # 원하는 형식으로 조회하고 싶은 경우 `SerializerMethodField`를 사용
+    #   사용자 정의 함수를 클래스 내에 정의한다.
+    # 조회하는 클래스와 상관관계가 없는 
 
     class Meta:
         model = Project
